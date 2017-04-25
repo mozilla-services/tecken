1a51837ac0a1bb16a775d258be8b3a8533146f2c
Peter Bengtsson <peterbe@mozilla.com>
2017-04-25 14:49:16 -0400

    fixes bug 1359505 - tecken favicon (#50)

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ Now a development server should be available at `http://localhost:8000`.
 To test the symbolication run:
 
     curl -d '{"stacks":[[[0,11723767],[1, 65802]]],"memoryMap":[["xul.pdb","44E4EC8C2F41492B9369D6B9A059577C2"],["wntdll.pdb","D74F79EB1F8D4A45ABCD2F476CCABACC2"]],"version":4}' http://localhost:8000
+
+
+The Logo
+--------
+
+![logo](logo.png "The Logo")
+
+The [logo](https://www.iconfinder.com/icons/118754/ampersand_icon) comes from
+[P.J. Onori](http://www.somerandomdude.com/) and is licensed under
+[Attribution-Non-Commercial 3.0 Netherlands](http://creativecommons.org/licenses/by-nc/3.0/nl/deed.en_GB).
--- tecken/urls.py
+++ tecken/urls.py
@@ -1,9 +1,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
 from django.conf import settings
 from django.conf.urls import include, url
-# from django.contrib.auth.decorators import login_required
 from django.views import generic, static
 
 from . import views
@@ -20,11 +22,15 @@ urlpatterns = [
         include('tecken.symbolicate.urls', namespace='symbolicate')
     ),
 
-    # contribute.json url
     url(
         r'^(?P<path>contribute\.json)$',
         static.serve,
         {'document_root': settings.BASE_DIR}
+    ),
+    url(
+        r'^(?P<path>favicon.ico)$',
+        static.serve,
+        {'document_root': os.path.join(settings.BASE_DIR, 'favicons')}
     ),
     url(r'^404/$', generic.TemplateView.as_view(template_name='404.html')),
     url(r'^500/$', generic.TemplateView.as_view(template_name='500.html')),
