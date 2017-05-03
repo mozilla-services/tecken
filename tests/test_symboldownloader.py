a6224c0a91fd552961eb14a0c41d448411af4d42
Peter Bengtsson <mail@peterbe.com>
2017-05-03 11:35:53 -0400

    better test coverage of symboldownloader

--- tests/test_symboldownloader.py
+++ tests/test_symboldownloader.py
@@ -7,9 +7,51 @@ from io import BytesIO
 from gzip import GzipFile
 
 import pytest
-# from requests.exceptions import ContentDecodingError
+from botocore.exceptions import ClientError
+from requests.exceptions import ContentDecodingError
+from requests.packages.urllib3.response import HTTPResponse
 
-from tecken.base.symboldownloader import SymbolDownloader, SymbolNotFound
+from tecken.base.symboldownloader import (
+    SymbolDownloader,
+    SymbolNotFound,
+    iter_lines,
+)
+
+
+def test_iter_lines():
+
+    class Stream:
+        def __init__(self, content):
+            self.left = content
+
+        def read(self, size):
+            if not self.left:
+                raise StopIteration
+            chunk = self.left[:size]
+            self.left = self.left[size:]
+            return chunk
+
+    lines = (
+        'Line 1\n'
+        'Line 2\n'
+        'Line 3\n'
+    )
+    stream = Stream(lines)
+    output = list(iter_lines(stream))
+    assert output == ['Line 1', 'Line 2', 'Line 3']
+
+    # Create it again because our little stream mock doesn't rewind
+    stream = Stream(lines)
+    output = list(iter_lines(stream, chunk_size=5))
+    assert output == ['Line 1', 'Line 2', 'Line 3']
+
+    stream = Stream(lines.strip())  # no trailing linebreak
+    output = list(iter_lines(stream))
+    assert output == ['Line 1', 'Line 2', 'Line 3']
+
+    stream = Stream(lines.strip())  # no trailing linebreak
+    output = list(iter_lines(stream, chunk_size=3))
+    assert output == ['Line 1', 'Line 2', 'Line 3']
 
 
 def test_has_public(requestsmock):
@@ -38,6 +80,22 @@ def test_has_public(requestsmock):
         '44E4EC8C2F41492B9369D6B9A059577C2',
         'xxx.sym'
     )
+
+
+def test_has_private_bubble_other_clienterrors(requestsmock):
+    urls = (
+        'https://s3.example.com/private/prefix/',
+    )
+    downloader = SymbolDownloader(urls)
+    # Expect this to raise a ClientError because the bucket ('private')
+    # doesn't exist. So boto3 would normally trigger a ClientError
+    # with a code 'Forbidden'.
+    with pytest.raises(ClientError):
+        downloader.has_symbol(
+            'xul.pdb',
+            '44E4EC8C2F41492B9369D6B9A059577C2',
+            'xul.sym'
+        )
 
 
 def test_has_private(s3_client):
@@ -257,7 +315,7 @@ def test_get_stream_private(s3_client):
         'xxx.sym'
     )
     with pytest.raises(SymbolNotFound):
-        list(stream)
+        next(stream)
 
 
 def test_get_stream_gzipped(s3_client):
@@ -295,6 +353,55 @@ def test_get_stream_gzipped(s3_client):
         'line 2',
         'line 3'
     ]
+
+
+def test_get_stream_gzipped_but_not_gzipped(s3_client):
+    s3_client.create_bucket(Bucket='private')
+    payload = (
+        b'line 1\n'
+        b'line 2\n'
+        b'line 3\n'
+    )
+    s3_client.put_object(
+        Bucket='private',
+        Key='prefix/xul.pdb/44E4EC8C2F41492B9369D6B9A059577C2/xul.sym',
+        Body=payload,
+        ContentEncoding='gzip',  # <-- note!
+    )
+    urls = (
+        'https://s3.example.com/private/prefix/',
+    )
+    downloader = SymbolDownloader(urls)
+    stream = downloader.get_symbol_stream(
+        'xul.pdb',
+        '44E4EC8C2F41492B9369D6B9A059577C2',
+        'xul.sym'
+    )
+    bucket_name, key = next(stream)
+    assert bucket_name == 'private'
+    assert key == 'prefix/xul.pdb/44E4EC8C2F41492B9369D6B9A059577C2/xul.sym'
+    # But when you start to stream it will realize that the file is not
+    # actually gzipped and SymbolDownloader will automatically just skip
+    # that file as if it doesn't exist.
+    with pytest.raises(SymbolNotFound):
+        next(stream)
+
+
+def test_get_stream_private_other_clienterrors(s3_client):
+    # Note that the bucket ('private') is not created in advance.
+    # That means you won't get a NoSuchKey when trying to get that
+    # bucket and key combo.
+    urls = (
+        'https://s3.example.com/private/prefix/',
+    )
+    downloader = SymbolDownloader(urls)
+    stream = downloader.get_symbol_stream(
+        'xul.pdb',
+        '44E4EC8C2F41492B9369D6B9A059577C2',
+        'xul.sym'
+    )
+    with pytest.raises(ClientError):
+        next(stream)
 
 
 def test_multiple_urls_public_then_private(requestsmock, s3_client):
@@ -407,28 +514,34 @@ def test_has_private_case_insensitive_debugid(s3_client):
     )
 
 
-# Commented out because there is no easy way to simulate that the
-# ContentDecodingError only happens when you start *reading* the stream.
-# def test_get_stream_public_content_encode_error(requestsmock):
-#     def cause_content_decoding_error(request, context):
-#         raise ContentDecodingError('bla!')
-#
-#     requestsmock.get(
-#         'https://s3.example.com/public/prefix/xul.pdb/'
-#         '44E4EC8C2F41492B9369D6B9A059577C2/xul.sym',
-#         status_code=200,
-#         text=cause_content_decoding_error
-#         #exc=ContentDecodingError,
-#     )
-#     urls = (
-#         'https://s3.example.com/public/prefix/?access=public',
-#     )
-#     downloader = SymbolDownloader(urls)
-#     stream = downloader.get_symbol_stream(
-#         'xul.pdb',
-#         '44e4ec8c2f41492b9369d6b9a059577c2',
-#         'xul.sym'
-#     )
-#     print(list(stream))
-#     print('STREAM', repr(stream))
-#     assert 0
+def test_get_stream_public_content_encode_error(requestsmock):
+
+    class BreakingStreamHTTPResponse(HTTPResponse):
+        def stream(self, *a, **kwargs):
+            raise ContentDecodingError('something terrible!')
+
+    requestsmock.get(
+        'https://s3.example.com/public/prefix/xul.pdb/'
+        '44E4EC8C2F41492B9369D6B9A059577C2/xul.sym',
+        raw=BreakingStreamHTTPResponse(
+            status=200
+        )
+    )
+    urls = (
+        'https://s3.example.com/public/prefix/?access=public',
+    )
+    downloader = SymbolDownloader(urls)
+    stream = downloader.get_symbol_stream(
+        'xul.pdb',
+        '44e4ec8c2f41492b9369d6b9a059577c2',
+        'xul.sym'
+    )
+    # Because the URL exists (hence the 200 OK), but when you start
+    # streaming it, it realizes it's there's something wrong with the
+    # content encoding, it captures that and consider this symbol
+    # not found.
+    # I.e. unable to stream its content is as bad as the file not existing.
+    # And because it's not found, the whole stream lookup is exhausted and
+    # it finally raises a SymbolNotFound error.
+    with pytest.raises(SymbolNotFound):
+        list(stream)
