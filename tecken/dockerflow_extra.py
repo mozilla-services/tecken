0e873b474709bbe03a75b4f90009f63976b51a14
Peter Bengtsson <peterbe@mozilla.com>
2017-06-13 14:03:47 -0400

    fixes bug 1364531 - Healthcheck should check that all SYMBOL_URLS are valid (#161)

--- tecken/dockerflow_extra.py
+++ tecken/dockerflow_extra.py
@@ -2,7 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
+from botocore.exceptions import ClientError, EndpointConnectionError
+
 from django.core import checks
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from dockerflow.django.checks import (
     ERROR_CANNOT_CONNECT_REDIS,
@@ -10,6 +13,7 @@ from dockerflow.django.checks import (
     ERROR_MISCONFIGURED_REDIS,
     ERROR_REDIS_PING_FAILED
 )
+from tecken.s3 import S3Bucket
 
 
 def check_redis_store_connected(app_configs, **kwargs):
@@ -38,4 +42,46 @@ def check_redis_store_connected(app_configs, **kwargs):
         if not result:
             msg = 'Redis ping failed'
             errors.append(checks.Error(msg, id=ERROR_REDIS_PING_FAILED))
+    return errors
+
+
+def check_s3_urls(app_configs, **kwargs):
+    errors = []
+    checked = []
+
+    def check_url(url, setting_key):
+        if url in checked:
+            return
+        bucket = S3Bucket(url)
+        if not bucket.private:
+            return
+        try:
+            bucket.s3_client.head_bucket(Bucket=bucket.name)
+        except ClientError as exception:
+            if exception.response['Error']['Code'] in ('403', '404'):
+                errors.append(checks.Error(
+                    f'Unable to connect to {url} (bucket={bucket.name!r}, '
+                    f'found in settings.{setting_key}) due to '
+                    f'ClientError ({exception.response!r})',
+                    id='tecken.health.E002'
+                ))
+            else:
+                raise
+        except EndpointConnectionError:
+            errors.append(checks.Error(
+                f'Unable to connect to {url} (bucket={bucket.name!r}, '
+                f'found in settings.{setting_key}) due to '
+                f'EndpointConnectionError',
+                id='tecken.health.E001'
+            ))
+        else:
+            checked.append(url)
+
+    for url in settings.SYMBOL_URLS:
+        check_url(url, 'SYMBOL_URLS')
+    if settings.UPLOAD_DEFAULT_URL:
+        check_url(settings.UPLOAD_DEFAULT_URL, 'UPLOAD_DEFAULT_URL')
+    for url in settings.UPLOAD_URL_EXCEPTIONS.values():
+        check_url(url, 'UPLOAD_URL_EXCEPTIONS')
+
     return errors
--- tests/test_dockerflow_extra.py
+++ tests/test_dockerflow_extra.py
@@ -2,8 +2,75 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
-from tecken.dockerflow_extra import check_redis_store_connected
+import pytest
+from botocore.exceptions import ClientError, EndpointConnectionError
+
+from tecken import dockerflow_extra
 
 
 def test_check_redis_store_connected_happy_path():
-    assert not check_redis_store_connected(None)
+    assert not dockerflow_extra.check_redis_store_connected(None)
+
+
+def test_check_s3_urls_happy_path(botomock, settings):
+
+    def mock_api_call(self, operation_name, api_params):
+        assert operation_name == 'HeadBucket'
+        # These come from settings.Test
+        fixture_bucket_names = (
+            'private',
+            'mybucket',
+            'peterbe-com',
+        )
+        assert api_params['Bucket'] in fixture_bucket_names
+        return {}
+
+    with botomock(mock_api_call):
+        assert not dockerflow_extra.check_s3_urls(None)
+
+
+def test_check_s3_urls_client_error(botomock, settings):
+
+    def mock_api_call(self, operation_name, api_params):
+        assert operation_name == 'HeadBucket'
+        if api_params['Bucket'] == 'mybucket':
+            response = {'Error': {'Code': '404', 'Message': 'Not found'}}
+            raise ClientError(response, operation_name)
+        return {}
+
+    with botomock(mock_api_call):
+        errors = dockerflow_extra.check_s3_urls(None)
+        assert errors
+        error, = errors
+        assert 'mybucket' in error.msg
+        assert 'ClientError' in error.msg
+
+
+def test_check_s3_urls_endpointconnectionerror(botomock, settings):
+
+    def mock_api_call(self, operation_name, api_params):
+        assert operation_name == 'HeadBucket'
+        if api_params['Bucket'] == 'mybucket':
+            raise EndpointConnectionError(endpoint_url='http://s3.example.com')
+        return {}
+
+    with botomock(mock_api_call):
+        errors = dockerflow_extra.check_s3_urls(None)
+        assert errors
+        error, = errors
+        assert 'mybucket' in error.msg
+        assert 'EndpointConnectionError' in error.msg
+
+
+def test_check_s3_urls_other_client_error(botomock, settings):
+
+    def mock_api_call(self, operation_name, api_params):
+        assert operation_name == 'HeadBucket'
+        if api_params['Bucket'] == 'mybucket':
+            response = {'Error': {'Code': '500', 'Message': 'Other'}}
+            raise ClientError(response, operation_name)
+        return {}
+
+    with botomock(mock_api_call):
+        with pytest.raises(ClientError):
+            dockerflow_extra.check_s3_urls(None)
