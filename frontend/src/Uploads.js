cbac11ecd0e2ccaa7583cc4a63cdd34c45b15abb
Peter Bengtsson <peterbe@mozilla.com>
2017-10-23 15:42:33 -0400

    indicate new rows in uploads frontend (#503)

--- frontend/src/Uploads.js
+++ frontend/src/Uploads.js
@@ -317,6 +317,7 @@ class Uploads extends React.PureComponent {
             filter={this.state.filter}
             updateFilter={this.updateFilter}
             resetAndReload={this.resetAndReload}
+            previousLatestUpload={this.previousLatestUpload}
           />
         )}
       </div>
@@ -415,6 +416,13 @@ class DisplayUploads extends React.PureComponent {
     this.props.resetAndReload(event)
   }
 
+  isNew = date => {
+    if (this.props.previousLatestUpload) {
+      return date > this.props.previousLatestUpload
+    }
+    return false
+  }
+
   render() {
     const { uploads, aggregates } = this.props
 
@@ -502,7 +510,10 @@ class DisplayUploads extends React.PureComponent {
                 <td>{upload.user.email}</td>
                 <td>{formatFileSize(upload.size)}</td>
                 <td>
-                  <DisplayDate date={upload.created_at} />
+                  <DisplayDate date={upload.created_at} />{' '}
+                  {this.isNew(upload.created_at) && (
+                    <span className="tag is-light">new</span>
+                  )}
                 </td>
                 <td>
                   {upload.completed_at ? (
