5ae69350aa98074939211af148c0670dae62ed8c
Peter Bengtsson <mail@peterbe.com>
2017-10-26 14:43:32 -0400

    correction

--- bin/build_frontend.sh
+++ bin/build_frontend.sh
@@ -5,17 +5,11 @@ set -eo pipefail
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Builds the React frontend (only if we are in CI)
+# Builds the React frontend, *only* if we are in CI.
 
-echo "CI: ${CI}"
-if [[ -n "${CI+set}" ]]; then
-    echo "YES -n STUFF"
-fi
-if [[ -z "${CI+check}" ]]; then
-    echo "YES -z CHECK STUFF"
-fi
-
-if [[ -n "${CI+set}" ]]; then
+# True if the environment variable 'CI' is NOT empty.
+# In CircleCI, this variable is "true" and in local dev, it's "".
+if [[ ! -z "${CI}" ]]; then
     # Because this is what create-react-app needs as a prefix
     export REACT_APP_SENTRY_PUBLIC_DSN=$FRONTEND_SENTRY_PUBLIC_DSN
 
