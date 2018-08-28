1004c3a2c955c37f67d83fd5e50d47639986ec11
Peter Bengtsson <peterbe@mozilla.com>
2018-08-28 13:46:56 -0400

    Timer on update_uploads_created_task (#1038)
    Part of #1037

--- tecken/upload/tasks.py
+++ tecken/upload/tasks.py
@@ -2,12 +2,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 
+import logging
+
+import markus
 from celery import shared_task
 from django.utils import timezone
 
 from tecken.upload.models import UploadsCreated
 
+logger = logging.getLogger("tecken")
+metrics = markus.get_metrics("tecken")
+
 
 @shared_task
 def update_uploads_created_task(date=None):
-    UploadsCreated.update(date or timezone.now().date())
+    date = date or timezone.now().date()
+    with metrics.timer("uploads_created_update"):
+        UploadsCreated.update(date)
+    logger.info(f"UploadsCreated updated for {date!r}")
