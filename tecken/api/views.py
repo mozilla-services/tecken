639a8ece0e70099803ad0a8942b0f1d77484d2ae
Peter Bengtsson <mail@peterbe.com>
2018-09-04 07:45:39 -0400

    blackfix

--- tecken/api/views.py
+++ tecken/api/views.py
@@ -625,7 +625,7 @@ def uploads_created_backfilled(request):
 
     if request.method == "POST":
         days = int(request.POST.get("days", 2))
-        force = request.POST.get('force', 'no') not in ('no', '0', 'false')
+        force = request.POST.get("force", "no") not in ("no", "0", "false")
         start = min_uploads.date()
         today = timezone.now().date()
         context["updated"] = []
