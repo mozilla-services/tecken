176981df9265b38726a2346267976b3e6fd2e350
Peter Bengtsson <peterbe@mozilla.com>
2018-11-01 10:34:47 -0400

    black the make-tag script (#1283)

--- bin/make-tag.py
+++ bin/make-tag.py
@@ -8,23 +8,19 @@ import subprocess
 
 
 def _check_output(*args, **kwargs):
-    return subprocess.check_output(*args, **kwargs).decode('utf-8').strip()
+    return subprocess.check_output(*args, **kwargs).decode("utf-8").strip()
 
 
 def run():
     # Let's make sure we're up-to-date
-    current_branch = _check_output(
-        'git rev-parse --abbrev-ref HEAD'.split()
-    )
-    if current_branch != 'master':
+    current_branch = _check_output("git rev-parse --abbrev-ref HEAD".split())
+    if current_branch != "master":
         print("Must be on the master branch to do this")
         return 1
 
     # The current branch can't be dirty
     try:
-        subprocess.check_call(
-            'git diff --quiet --ignore-submodules HEAD'.split()
-        )
+        subprocess.check_call("git diff --quiet --ignore-submodules HEAD".split())
     except subprocess.CalledProcessError:
         print(
             "Can't be \"git dirty\" when we're about to git pull. "
@@ -33,50 +29,47 @@ def run():
         return 2
 
     # Make sure we have all the old git tags
-    _check_output(
-        'git pull origin master --tags'.split(),
-        stderr=subprocess.STDOUT
-    )
+    _check_output("git pull origin master --tags".split(), stderr=subprocess.STDOUT)
 
     # We're going to use the last tag to help you write a tag message
-    last_tag, last_tag_message = _check_output([
-        'git',
-        'for-each-ref',
-        '--sort=-taggerdate',
-        '--count=1',
-        '--format',
-        '%(tag)|%(contents:subject)',
-        'refs/tags'
-    ]).split('|', 1)
+    last_tag, last_tag_message = _check_output(
+        [
+            "git",
+            "for-each-ref",
+            "--sort=-taggerdate",
+            "--count=1",
+            "--format",
+            "%(tag)|%(contents:subject)",
+            "refs/tags",
+        ]
+    ).split("|", 1)
 
-    print('\nLast tags was: {}'.format(last_tag))
-    if last_tag_message.count('\n') > 1:
-        print('Message:')
+    print("\nLast tags was: {}".format(last_tag))
+    if last_tag_message.count("\n") > 1:
+        print("Message:")
         print(last_tag_message)
     else:
-        print('Message: ', last_tag_message)
-    print('-' * 80)
+        print("Message: ", last_tag_message)
+    print("-" * 80)
 
     commits_since = _check_output(
-        'git log {last_tag}..HEAD --oneline'.format(last_tag=last_tag).split()
+        "git log {last_tag}..HEAD --oneline".format(last_tag=last_tag).split()
     )
-    print('Commits since last tag:')
+    print("Commits since last tag:")
     for commit in commits_since.splitlines():
-        print('\t', commit)
-    print('-' * 80)
+        print("\t", commit)
+    print("-" * 80)
 
     # Next, come up with the next tag name.
     # Normally it's today's date in ISO format with dots.
-    tag_name = datetime.datetime.utcnow().strftime('%Y.%m.%d')
+    tag_name = datetime.datetime.utcnow().strftime("%Y.%m.%d")
     # But is it taken, if so how many times has it been taken before?
-    existing_tags = _check_output([
-        'git', 'tag', '-l', '{}*'.format(tag_name)
-    ]).splitlines()
+    existing_tags = _check_output(
+        ["git", "tag", "-l", "{}*".format(tag_name)]
+    ).splitlines()
     if existing_tags:
-        count_starts = len(
-            [x for x in existing_tags if x.startswith(tag_name)]
-        )
-        tag_name += '-{}'.format(count_starts + 1)
+        count_starts = len([x for x in existing_tags if x.startswith(tag_name)])
+        tag_name += "-{}".format(count_starts + 1)
 
     # Now we need to figure out what's been
     message = input("Tag message? (Optional, else all commit messages) ")
@@ -84,20 +77,13 @@ def run():
         message = commits_since
 
     # Now we can create the tag
-    subprocess.check_call([
-        'git',
-        'tag',
-        '-s',
-        '-a', tag_name,
-        '-m', message
-    ])
+    subprocess.check_call(["git", "tag", "-s", "-a", tag_name, "-m", message])
 
     # Let's push this now
-    subprocess.check_call(
-        'git push origin master --tags'.split()
-    )
+    subprocess.check_call("git push origin master --tags".split())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     sys.exit(run())
