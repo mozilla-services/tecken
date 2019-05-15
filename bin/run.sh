5687bc225c0cb0c6adec53fffe7e6a18a1cc601d
Will Kahn-Greene <willkg@mozilla.com>
2019-05-15 13:28:50 -0400

    Disable saving coverage report
    This doesn't work in Docker on Linux because of differences in uid and fs
    permissions and all that. We'd need to rework how everything gets run
    and which uid/gid it gets run as to get this to work again. In the meantime,
    I'm just going to shut it off.

--- bin/run.sh
+++ bin/run.sh
@@ -77,11 +77,13 @@ case $1 in
     coverage erase
     coverage run -m py.test "${@:2}"
     coverage report -m
-    if [[ -z ${CI+check} ]]; then  # when doing local `make test`
-      # generate code coverage to disk
-      coverage html --skip-covered
-    fi
-    # Temporarily disabled. The team is small and codecov's report inside
+    # FIXME(willkg): this doesn't work with fs permissions on Linux
+    # if [[ -z ${CI+check} ]]; then  # when doing local `make test`
+    #   # generate code coverage to disk
+    #   coverage html --skip-covered
+    # fi
+
+    # FIXME(peterbe) The team is small and codecov's report inside
     # pull requests (as comments) is more noise than help.
     # Also, code coverage is mostly useful when contributors help and
     # add more code without adding tests to cover.
