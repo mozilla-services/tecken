cb65507c89dc3ada1c225d64a6c071a27618fa1b
Will Kahn-Greene <willkg@mozilla.com>
2019-06-05 13:42:41 -0400

    Overhaul make-tag
    This fixes a bunch of things in make-tag to make it easier to use:
    
    1. it figures out the correct remote since we don't all have "origin"
       as the remote name
    2. it tells you more about what it's going to do before it does it
    3. it prompts you before it does things--this gives you a chance to
       say, "OH NO!!! DON'T DO THAT!!!" before triggering a deploy

--- bin/make-tag.py
+++ bin/make-tag.py
@@ -5,17 +5,30 @@
 
 import datetime
 import subprocess
+import sys
 
 
 def _check_output(*args, **kwargs):
     return subprocess.check_output(*args, **kwargs).decode("utf-8").strip()
 
 
+def get_remote_name():
+    # Figure out remote to push tag to
+    remote_output = _check_output("git remote -v".split())
+
+    for line in remote_output.splitlines():
+        line = line.split("\t")
+        if "mozilla-services" in line[1]:
+            return line[0]
+
+    raise Exception("Can't figure out remote name for mozilla-services.")
+
+
 def run():
-    # Let's make sure we're up-to-date
+    # Let's make sure we're up-to-date and on master branch
     current_branch = _check_output("git rev-parse --abbrev-ref HEAD".split())
     if current_branch != "master":
-        print("Must be on the master branch to do this")
+        print(f"Must be on the master branch to do this (not {current_branch})")
         return 1
 
     # The current branch can't be dirty
@@ -31,34 +44,34 @@ def run():
     # Make sure we have all the old git tags
     _check_output("git pull origin master --tags".split(), stderr=subprocess.STDOUT)
 
+    # Get the last tag
     # We're going to use the last tag to help you write a tag message
-    last_tag, last_tag_message = _check_output(
+    last_tag = _check_output(
         [
             "git",
             "for-each-ref",
             "--sort=-taggerdate",
             "--count=1",
             "--format",
-            "%(tag)|%(contents:subject)",
+            "%(tag)",
             "refs/tags",
         ]
-    ).split("|", 1)
+    )
+    last_tag_message = _check_output(["git", "tag", "-l", "--format=%(tag) %(contents)", last_tag])
 
-    print("\nLast tags was: {}".format(last_tag))
-    if last_tag_message.count("\n") > 1:
-        print("Message:")
-        print(last_tag_message)
-    else:
-        print("Message: ", last_tag_message)
-    print("-" * 80)
+    print(">>> Last tag was: {}".format(last_tag))
+    print(">>> Message:")
+    print(last_tag_message)
+    print("=" * 80)
 
-    commits_since = _check_output(
+    message = _check_output(
         "git log {last_tag}..HEAD --oneline".format(last_tag=last_tag).split()
     )
-    print("Commits since last tag:")
-    for commit in commits_since.splitlines():
-        print("\t", commit)
-    print("-" * 80)
+    # Take out Merge commit lines
+    message = '\n'.join([
+        line for line in message.splitlines()
+        if not line[8:].startswith('Merge pull request')
+    ])
 
     # Next, come up with the next tag name.
     # Normally it's today's date in ISO format with dots.
@@ -72,18 +85,24 @@ def run():
         tag_name += "-{}".format(count_starts + 1)
 
     # Now we need to figure out what's been
-    message = input("Tag message? (Optional, else all commit messages) ")
-    if not message:
-        message = commits_since
+    print(">>> New tag: %s" % tag_name)
+    print(">>> Tag message:")
+    print("=" * 80)
+    print(message)
+    print("=" * 80)
 
-    # Now we can create the tag
-    subprocess.check_call(["git", "tag", "-s", "-a", tag_name, "-m", message])
+    remote_name = get_remote_name()
 
-    # Let's push this now
-    subprocess.check_call("git push origin master --tags".split())
+    # Create tag
+    input(">>> Ready to tag \"{}\"? Ctrl-c to cancel".format(tag_name))
+    print(">>> Creating tag...")
+    subprocess.check_call(["git", "tag", "-s", tag_name, "-m", message])
+
+    # Push tag
+    input(">>> Ready to push to remote \"{}\"? Ctrl-c to cancel".format(remote_name))
+    print(">>> Pushing...")
+    subprocess.check_call(["git", "push", "--tags", remote_name, tag_name])
 
 
 if __name__ == "__main__":
-    import sys
-
     sys.exit(run())
