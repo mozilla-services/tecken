581e3adec53154e3e1e1501d66d0a52f607784fa
Will Kahn-Greene <willkg@mozilla.com>
2020-11-02 16:39:23 -0500

    bug 1636210: improve Eliot docs

--- bin/run_eliot_web.sh
+++ bin/run_eliot_web.sh
@@ -5,16 +5,17 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # default variables
-: "${ELIOT_PORT:=8000}"
+: "${ELIOT_GUNICORN_PORT:=8000}"
 : "${ELIOT_GUNICORN_WORKERS:=1}"
 : "${ELIOT_GUNICORN_TIMEOUT:=300}"
+: "${ELIOT_GUNICORN_CMD_PREFIX:=}"
 
-(set -o posix; set) | grep ELIOT
+(set -o posix; set) | grep ELIOT_GUNICORN
 
 cd /app/eliot-service/
 
-${CMD_PREFIX} gunicorn \
-    --bind 0.0.0.0:"${ELIOT_PORT}" \
+${ELIOT_GUNICORN_CMD_PREFIX} gunicorn \
+    --bind 0.0.0.0:"${ELIOT_GUNICORN_PORT}" \
     --timeout "${ELIOT_GUNICORN_TIMEOUT}" \
     --workers "${ELIOT_GUNICORN_WORKERS}" \
     --preload \
