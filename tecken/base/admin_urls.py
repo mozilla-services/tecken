9c9dff3432943fd127c3633bf594aa3fd86a11d2
Will Kahn-Greene <willkg@mozilla.com>
2022-01-27 12:12:46 -0500

    bug 1703253: swap url out for re_path
    django.conf.urls.url isn't a thing anymore, so this swaps it out for
    re_path.

--- tecken/base/admin_urls.py
+++ tecken/base/admin_urls.py
@@ -2,12 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from tecken.base import admin
 
 
 app_name = "siteadmin"
 urlpatterns = [
-    url("^sitestatus/$", admin.site_status, name="site_status"),
+    re_path("^sitestatus/$", admin.site_status, name="site_status"),
 ]
