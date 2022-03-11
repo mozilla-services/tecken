6107dbe8e3142e3e43bd6d4cdf72aea006bc2b59
Will Kahn-Greene <willkg@mozilla.com>
2022-03-11 14:13:37 -0500

    bug 1753278: add sentry-wrap.py and run_migrations.sh scripts
    This adds a script for running migrations for Tecken such that it's
    wrapped in sentry-error-reporting goodness.

---
+++ bin/run_migrations.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Usage: bin/run_migrations.sh [--fix]
+#
+# This script runs migrations. Run this in a crontabber docker container.
+#
+# Note: This should be called from inside a container.
+
+set -euo pipefail
+
+PRECMD=""
+
+# If SENTRY_DSN is defined, then enable the sentry-cli bash hook which will
+# send errors to sentry.
+if [ -n "${SENTRY_DSN:-}" ]; then
+    echo "SENTRY_DSN defined--enabling sentry."
+    PRECMD="python bin/sentry-wrap.py wrap-process --timeout=600 --"
+else
+    echo "SENTRY_DSN not defined--not enabling sentry."
+fi
+
+echo "starting run_migrations.sh: $(date)"
+
+# Run Django migrations
+${PRECMD} python manage.py migrate --no-input
