b5012987d8a261116d6817c86467091561353f6c
Will Kahn-Greene <willkg@mozilla.com>
2022-04-13 17:08:42 -0400

    bug 1764622: add missing license headers and fix existing ones

--- systemtests/bin/make-stacks.py
+++ systemtests/bin/make-stacks.py
@@ -2,7 +2,7 @@
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 # Fetches processed crash data for given crash ids and generates
 # stacks for use with the Symbolication API. This has two modes:
