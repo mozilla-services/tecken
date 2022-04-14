55e7bc1d9d0dd2f55ad5cc0745633183b76667c5
Will Kahn-Greene <willkg@mozilla.com>
2022-04-14 11:28:43 -0400

    bug 1764622: add explanation and usage text to header

--- bin/license_check.py
+++ bin/license_check.py
@@ -4,6 +4,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# Usage: python bin/license_check.py
+
+# This checks files for a license header.
+
 import argparse
 import pathlib
 import subprocess
