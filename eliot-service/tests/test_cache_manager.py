e2fa4d59dee712991e54a776e376da90f8239b0b
Will Kahn-Greene <willkg@mozilla.com>
2022-09-02 07:14:40 -0400

    Update dependencies
    * boto3: 1.24.44 -> 1.24.65
    * botocore: 1.27.64 -> 1.27.65
    * jsonschema: 4.9.1 -> 4.15.0
    * requests-mock: 1.9.3 -> 1.10.0
    * sentry-sdk: 1.9.5 -> 1.9.7
    * urllib3: 1.26.11 -> 1.26.12
    * werkzeug: 2.2.1 -> 2.2.2

--- eliot-service/tests/test_cache_manager.py
+++ eliot-service/tests/test_cache_manager.py
@@ -493,8 +493,8 @@ BROKEN_EVENT = {
     "server_name": "testnode",
     "sdk": {
         "name": "sentry.python",
-        "version": "1.9.5",
-        "packages": [{"name": "pypi:sentry-sdk", "version": "1.9.5"}],
+        "version": "1.9.7",
+        "packages": [{"name": "pypi:sentry-sdk", "version": "1.9.7"}],
         "integrations": [
             "argv",
             "atexit",
