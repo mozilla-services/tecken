3245d20f358b52117916ba434328868cfb0ade83
Will Kahn-Greene <willkg@mozilla.com>
2023-05-24 12:11:31 -0400

    bug 1814436: improve cleanse* commands to emit timings
    This cleans up the output for the cleanse* commands and adds timings.

--- tecken/upload/management/commands/cleanse_upload.py
+++ tecken/upload/management/commands/cleanse_upload.py
@@ -8,6 +8,7 @@ from django.core.management.base import BaseCommand
 from django.db import connection, reset_queries
 from django.utils import timezone
 
+from tecken.libtiming import record_timing
 from tecken.upload.models import Upload, FileUpload
 
 
@@ -43,7 +44,7 @@ class Command(BaseCommand):
 
         else:
             # NOTE(willkg): We use ._raw_delete() instead of .delete() here because
-            # .dete() causes a SELECT which pulls back all the data of the stuff to be
+            # .delete() causes a SELECT which pulls back all the data of the stuff to be
             # deleted which is really intense and makes it not possible to run these
             # queries in prod. It does that to prevent an integrity error because
             # FileUpload has on_delete SET_NULL.
@@ -58,11 +59,13 @@ class Command(BaseCommand):
             upload_count = uploads._raw_delete(using=del_query.db)
 
         self.stdout.write(
-            f"cleanse_upload: try={is_try}, cutoff={cutoff.date()}: "
-            f"deleted upload={upload_count}, fileupload={fileupload_count}"
+            f">>> try={is_try}, cutoff={cutoff.date()}: "
+            + f"deleted upload={upload_count}, fileupload={fileupload_count}"
         )
 
     def handle(self, *args, **options):
+        self.stdout.write("cleanse_upload:")
+
         # NOTE(willkg): if DEBUG=False, there's nothing to reset and this is a no-op
         reset_queries()
 
@@ -71,22 +74,25 @@ class Command(BaseCommand):
         today.replace(hour=0, minute=0, second=0, microsecond=0)
 
         if is_dry_run:
-            self.stdout.write("cleanse_upload: THIS IS A DRY RUN.")
+            self.stdout.write(">>> THIS IS A DRY RUN.")
 
-        upload_count = Upload.objects.all().count()
-        fileupload_count = FileUpload.objects.all().count()
-        self.stdout.write(
-            f"cleanse_upload: count before cleansing: "
-            f"upload={upload_count}, fileupload={fileupload_count}"
-        )
+        with record_timing("count", self.stdout):
+            upload_count = Upload.objects.all().count()
+            fileupload_count = FileUpload.objects.all().count()
+            self.stdout.write(
+                ">>> count before cleansing: "
+                + f"upload={upload_count}, fileupload={fileupload_count}"
+            )
 
-        # First cleanse try records
-        try_cutoff = today - datetime.timedelta(days=TRY_RECORD_AGE_CUTOFF)
-        self.delete_records(is_dry_run=is_dry_run, is_try=True, cutoff=try_cutoff)
+        with record_timing("delete", self.stdout):
+            # First cleanse try records
+            try_cutoff = today - datetime.timedelta(days=TRY_RECORD_AGE_CUTOFF)
+            self.delete_records(is_dry_run=is_dry_run, is_try=True, cutoff=try_cutoff)
 
-        # Now cleanse regular records
-        cutoff = today - datetime.timedelta(days=REGULAR_RECORD_AGE_CUTOFF)
-        self.delete_records(is_dry_run=is_dry_run, is_try=False, cutoff=cutoff)
+        with record_timing("delete", self.stdout):
+            # Now cleanse regular records
+            cutoff = today - datetime.timedelta(days=REGULAR_RECORD_AGE_CUTOFF)
+            self.delete_records(is_dry_run=is_dry_run, is_try=False, cutoff=cutoff)
 
         # NOTE(willkg): this only prints the SQL when DEBUG=True
         for query in connection.queries:
