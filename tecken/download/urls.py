d1c996588242103826f717f31b13a7e3bd4ca6a3
Will Kahn-Greene <willkg@mozilla.com>
2023-06-01 11:05:04 -0400

    bug 1834822: remove legacy product download url scheme

--- tecken/download/urls.py
+++ tecken/download/urls.py
@@ -3,9 +3,8 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from django.urls import register_converter, path
-from django.conf import settings
 
-from . import views
+from tecken.download import views
 
 
 class _Converter:
@@ -20,17 +19,7 @@ class MixedCaseHexConverter(_Converter):
     regex = "[0-9A-Fa-f]+"
 
 
-class LegacyProductPrefixesConverter(_Converter):
-    """This is a relic from the past when the symbols were prefixed by
-    the products we had at the time.
-    To be safe (Jan 2018), let's just keep this around as a valid URL.
-    """
-
-    regex = "({})".format("|".join(settings.DOWNLOAD_LEGACY_PRODUCTS_PREFIXES))
-
-
 register_converter(MixedCaseHexConverter, "hex")
-register_converter(LegacyProductPrefixesConverter, "legacyproduct")
 
 
 app_name = "download"
@@ -42,11 +31,6 @@ urlpatterns = [
         "try/<str:symbol>/<hex:debugid>/<str:filename>",
         views.download_symbol_try,
         name="download_symbol_try",
-    ),
-    path(
-        "<legacyproduct>/<str:symbol>/<hex:debugid>/<str:filename>",
-        views.download_symbol_legacy,
-        name="download_symbol_legacy",
     ),
     path(
         "<str:symbol>/<hex:debugid>/<str:filename>",
