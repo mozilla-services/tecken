80e469c1f1c5f4325c349dab32a9e8ba5f452bb4
Will Kahn-Greene <willkg@mozilla.com>
2023-06-23 18:53:36 -0400

    Add comments to entrypoint.sh script

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,6 +3,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Entrypoint script for the tecken docker image.
+#
+# Usage: entrypoint.sh SERVICE
+#
+# web: runs the webapp
+#
+# bash: if no additional arguments, runs the bash shell; if additional
+# arguments, runs that.
 
 set -eo pipefail
 
