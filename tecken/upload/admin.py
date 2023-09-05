d5fb2efbe753551f484f141abc65adac407381f4
Will Kahn-Greene <willkg@mozilla.com>
2023-09-05 16:23:57 -0400

    bug 1746940: add FileUploadAdmin and UploadAdmin to Django admin
    This makes it possible to edit data in the future.
    
    This also improves the models in regards to Django best practices.

---
+++ tecken/upload/admin.py
@@ -0,0 +1,44 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from django.contrib import admin
+
+from tecken.upload.models import Upload, FileUpload
+
+
+@admin.register(Upload)
+class UploadAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    search_fields = ["user.email", "bucket_name"]
+    list_display = [
+        "id",
+        "user_email",
+        "try_symbols",
+        "download_by_upload",
+        "bucket_name",
+        "size",
+        "created_at",
+    ]
+
+    view_on_site = True
+
+    def user_email(self, obj):
+        return obj.user.email
+
+    def download_by_upload(self, obj):
+        return bool(obj.download_url)
+
+
+@admin.register(FileUpload)
+class FileUploadAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    search_fields = ["key"]
+    list_display = ["id", "upload_id", "bucket_name", "key", "size", "created_at"]
+
+    view_on_site = True
+
+    def upload_id(self, obj):
+        if obj.upload:
+            return obj.upload.id
+        return -1
