e1992468b7a756387b2fe03d510be14e35378cd8
Bianca Danforth <biancadanforth@gmail.com>
2024-01-05 11:18:49 -0500

    bug-1827507: implement GCP dev environment (#2868)
    Because:
    * We want to use an emulator for local development and CI, since it is faster among other benefits.
    This commit:
    - Adds a new `gcs-emulator` service, linking it where `localstack` is also linked
    - Creates a new GCS CLI for local dev with the same commands as the S3 CLI
      - create bucket
      - delete bucket
      - list buckets
      - list objects 
    - Calls the new CLI in `make setup`
    - Updates docs
    
    Prior art this commit is based on:
     * [fake-gcs-server example Python usage](https://github.com/fsouza/fake-gcs-server/blob/main/examples/python/python.py)
       - For how to initialize the google_cloud_storage client using a dockerized fake-gcs-server
     * [Willkg's s3_cli.py script for the AWS S3 localstack emulator](https://github.com/mozilla-services/tecken/pull/2556)
       - For what the gcs_cli command and subcommand interface should look like
     * [jwhitlock's prior art for adding a fork of fake-gcs-server to Tecken in 2019](https://github.com/mozilla-services/tecken/pull/1830/files#diff-7edb779e7edd96899e8dc0656376f894d98d631539742ecce68a964a7dd5edfaR190-R198)
       - For how to make insecure https requests to the GCS emulator and suppress warnings

--- bin/setup-services.sh
+++ bin/setup-services.sh
@@ -10,6 +10,12 @@ set -eo pipefail
 python bin/s3_cli.py delete "${UPLOAD_DEFAULT_URL}"
 python bin/s3_cli.py create "${UPLOAD_DEFAULT_URL}"
 
+# Set up GCS
+# FIXME bug 1827506: update argument as needed once GCS is
+# implemented in the source code.
+python bin/gcs_cli.py delete publicbucket
+python bin/gcs_cli.py create publicbucket
+
 # Set up db
 python bin/db.py drop || true
 python bin/db.py create
