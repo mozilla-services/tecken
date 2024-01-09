mozilla-services/tecken

commit 23daea485d5b4b00fb6bbee517c9a3d03a2e2051

bug 1746940: fix upload-sym file for Windows modules

bin/upload-sym.py
@@ -49,8 +49,12 @@ def upload_sym_file(ctx, auth_token, base_url, symfile):
     parts = firstline.split(" ")
     debugid = parts[3].strip()
     debugfilename = parts[4].strip()
+    if debugfilename.endswith(".pdb"):
+        sym_file = debugfilename[:-4] + ".sym"
+    else:
+        sym_file = debugfilename
 
-    path = f"{debugfilename}/{debugid}/{debugfilename}.sym"
+    path = f"{debugfilename}/{debugid}/{sym_file}"
     with tempfile.TemporaryDirectory(prefix="symbols") as tmpdirname:
         zip_filename = datetime.datetime.now().strftime("symbols_%Y%m%d_%H%M%S.zip")
         zip_path = os.path.join(tmpdirname, zip_filename)

commit 6866492b5b3665fb508b366c7c2a9fd009baad7e

Fix upload-sym.py sym_file generation
This fixes a bug introduced in September 2023 in 23daea485d, where
I fixed the code to generate the right sym_file filename for Windows,
but broke it for Linux/macOS. This fixes Linux/macOS.

bin/upload-sym.py
@@ -52,7 +52,7 @@ def upload_sym_file(ctx, auth_token, base_url, symfile):
     if debugfilename.endswith(".pdb"):
         sym_file = debugfilename[:-4] + ".sym"
     else:
-        sym_file = debugfilename
+        sym_file = debugfilename + ".sym"
 
     path = f"{debugfilename}/{debugid}/{sym_file}"
     with tempfile.TemporaryDirectory(prefix="symbols") as tmpdirname: