9e6670a8fb11dc7d5040a5f1078658bb33113c54
Will Kahn-Greene <willkg@mozilla.com>
2024-01-29 13:56:25 -0500

    Force errors when building docs to return non-0 exit code

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,14 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    # We want to include images in drawio directory, but not the documentation for
+    # updating those images.
+    "drawio/**",
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
