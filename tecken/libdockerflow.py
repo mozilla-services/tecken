cf8789145ea9a0df1f16a89aa30ea476d2b0bdd0
Sven Marnach <sven@mozilla.com>
2024-06-05 16:41:45 +0200

    bug-1900789: Remove direct reference to StorageBucket from libdockerflow.

--- tecken/libdockerflow.py
+++ tecken/libdockerflow.py
@@ -6,24 +6,19 @@ import json
 from pathlib import Path
 
 from django.core import checks
-from django.conf import settings
 
-from tecken.storage import StorageBucket, StorageError
+from tecken.storage import StorageError
+from tecken.base.symbolstorage import normal_storage
 
 
 def check_storage_urls(app_configs, **kwargs):
     errors = []
-    checked = []
-
-    def check_url(url, setting_key):
-        if url in checked:
-            return
-        bucket = StorageBucket(url)
+    for source in normal_storage().sources:
         try:
-            if not bucket.exists():
+            if not source.exists():
                 errors.append(
                     checks.Error(
-                        f"Unable to connect to {url} (bucket={bucket.name!r}), "
+                        f"Unable to connect to source {source.name!r}, "
                         f"because bucket not found",
                         id="tecken.health.E001",
                     )
@@ -31,17 +26,11 @@ def check_storage_urls(app_configs, **kwargs):
         except StorageError as error:
             errors.append(
                 checks.Error(
-                    f"Unable to connect to {url} (bucket={bucket.name!r}), "
+                    f"Unable to connect to source {source.name!r}, "
                     f"due to {error.backend_msg}",
                     id="tecken.health.E002",
                 )
             )
-        else:
-            checked.append(url)
-
-    for url in settings.SYMBOL_URLS:
-        check_url(url, "SYMBOL_URLS")
-
     return errors
 
 
