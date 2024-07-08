43953b6ba6249d2e773ee832e2548df75e6a2645
Sven Marnach <sven@mozilla.com>
2024-07-08 20:37:38 +0200

    bug-1827506: Make download tests run with both GCS and S3 backends.

--- tecken/tests/test_symbolstorage.py
+++ tecken/tests/test_symbolstorage.py
@@ -5,7 +5,7 @@
 import pytest
 
 from tecken.base.symbolstorage import SymbolStorage
-from tecken.tests.utils import Upload
+from tecken.tests.utils import UPLOADS
 
 
 @pytest.mark.parametrize(
@@ -32,17 +32,14 @@ def test_make_url_path(symbol, debugid, filename, expected):
 
 @pytest.mark.parametrize("lower_case_debug_id", [False, True])
 def test_get_metadata(symbol_storage, lower_case_debug_id):
-    backend = symbol_storage.get_upload_backend(False)
-    upload = Upload.uncompressed(
-        key="xul.pdb/44E4EC8C2F41492B9369D6B9A059577C2/xul.sym", body=b"abc123"
-    )
-    upload.upload(backend)
-    debug_id = "44E4EC8C2F41492B9369D6B9A059577C2"
+    upload = UPLOADS["compressed"]
+    upload.upload(symbol_storage)
+    debug_id = upload.debug_id
     if lower_case_debug_id:
         debug_id = debug_id.lower()
     metadata = symbol_storage.get_metadata("xul.pdb", debug_id, "xul.sym")
     assert metadata.download_url == (
-        f"{backend.url}/v1/xul.pdb/44E4EC8C2F41492B9369D6B9A059577C2/xul.sym"
+        f"{upload.backend.url}/v1/xul.pdb/44E4EC8C2F41492B9369D6B9A059577C2/xul.sym"
     )
     assert not symbol_storage.get_metadata(
         "xxx.pdb", "44E4EC8C2F41492B9369D6B9A059577C2", "xxx.sym"
