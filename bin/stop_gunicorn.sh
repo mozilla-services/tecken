a8c1fbadc1e893af1f2926ffcc9cfc99049c0d2f
Will Kahn-Greene <willkg@mozilla.com>
2024-12-09 09:31:50 -0500

    Cleanup fixes to bin/stop_gunicorn.sh

--- bin/stop_gunicorn.sh
+++ bin/stop_gunicorn.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Send the TERM signal to gunicorn to trigger a graceful shutdown, and wait for it to exit.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Send the TERM signal to gunicorn to trigger a graceful shutdown, and wait for
+# it to exit.
 
 set -euo pipefail
 
-pid=`cat /tmp/gunicorn.pid`
+pid=$(cat /tmp/gunicorn.pid)
 
 if [ -n "$pid" ]; then
     echo "Sending TERM signal to gunicorn."
     kill -TERM "$pid"
     echo "Waiting for gunicorn to exit..."
-    while kill -0 $pid 2>/dev/null; do
+    while kill -0 "$pid" 2>/dev/null; do
         sleep 1
     done
 fi
